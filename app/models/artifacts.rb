c0cfdefe7f6a1e85a15814eb69f8803edd45026e
Steven Hammond <shammond@northpub.com>
2012-10-13 07:34:46 -0400

    rename Artifacts to Artifact

--- app/models/artifacts.rb
+++ app/models/artifacts.rb
@@ -1,6 +1,8 @@
-class Artifacts < ActiveRecord::Base
+class Artifact < ActiveRecord::Base
   attr_accessible :accession_number, :std_term, :alt_name, :prob_date, :min_date, :max_date,
     :artist, :school_period, :geoloc, :origin, :materials, :measure, :weight, :comments,
     :bibliography, :published_refs, :label_text, :old_labels, :exhibit_history, :description,
     :marks, :public_loc, :status
+
+    validates_uniqueness_of :accession_number
 end
