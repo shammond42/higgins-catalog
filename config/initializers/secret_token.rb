798206ce0a19ed254a5ff6f9ac19e8d44e99306f
Steven Hammond <shammond@northpub.com>
2012-10-12 22:38:30 -0400

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HigginsCatalog::Application.config.secret_token = '80839fae090945c2da18844ed009097d4ced15ea98c912fa861aaf549720bb6b722388042bb1e68e21227a78742d0831bcbea34d28613024f15bb07fe1cc3bff'
