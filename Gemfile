a35de2255a06d135ce93048d676e304535e09102
Steven Hammond <shammond@northpub.com>
2012-10-14 10:22:59 -0400

    Styled artifact show page.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.8'
 
 gem 'pg'
 gem 'haml-rails'
+gem 'RedCloth'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/helpers/artifacts_helper.rb
+++ app/helpers/artifacts_helper.rb
@@ -7,4 +7,17 @@ module ArtifactsHelper
     text = "#{low} &ndash; #{high}".html_safe unless text.present?
     link_to text, temportal_facet_path(params, low, high)
   end
+
+  def display_fields
+    [
+      {field: 'accession_number', label: 'Accession Number'},
+      {field: 'origin', label: 'Origin'},
+      {field: 'prob_date', label: 'Probable Date'},
+      {field: 'artist', label: 'Artist'},
+      {field: 'school_period', label: 'School/Period'},
+      {field: 'materials', label: 'Materials'},
+      {field: 'measure', label: 'Measure'},
+      {field: 'weight', label: 'Weight'}
+    ]
+  end
 end
