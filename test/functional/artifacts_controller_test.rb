e6a1a2cc73d586bd75ba5c14861e73468cc6355d
Steven Hammond <shammond@northpub.com>
2012-10-21 12:51:17 -0400

    More artifact controller tests.

--- test/functional/artifacts_controller_test.rb
+++ test/functional/artifacts_controller_test.rb
@@ -8,9 +8,22 @@ class ArtifactsControllerTest < ActionController::TestCase
 
     get :index
     assert_not_nil assigns[:artifacts]
-    # Need to mock out Elastic search because its index is affecting test results
+    # TODO: Need to mock out Elastic search because its index is affecting test results
     # assert_equal Artifact.count, assigns[:artifacts].size
     assert_response :success
     assert_template :index
+  end
+
+  test 'look at a specific artifact' do
+    artifact = FactoryGirl.create(:artifact)
+
+    get :show, :id => artifact.to_param
+    assert_response :success
+    assert_template :show
+    assert_not_nil assigns[:artifact]    
+  end
+
+  test 'daily artifiact' do
+    
   end
 end
