8eb434ea8b6f3c0eb4e7b068e236184bbcd44925
Steven Hammond <shammond@northpub.com>
2012-10-22 08:36:00 -0400

    Creater a question functional test.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,9 @@
 class QuestionsController < ApplicationController
-  # def index
-  #   @questions = Question.unanswered.order('created_at asc')
-  # end
+  before_filter :authenticate_user!, except: [:create]
+
+  def index
+    @questions = Question.unanswered.order('created_at asc')
+  end
 
   def create
     @artifact = Artifact.find_by_accession_number(Artifact.from_param(params[:artifact_id]))
--- test/functional/questions_controller_test.rb
+++ test/functional/questions_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'ask a question' do
+    artifact = FactoryGirl.create(:artifact)
+    redirect_path = artifact_url(artifact)
+    request.env["HTTP_REFERER"] = redirect_path
+
+    old_question_count = Question.count
+
+    post :create, artifact_id: artifact.to_param, question: {question: 'What is this?', nickname: 'Bilbo',
+      email: 'bbaggins@fellowship.org'}
+
+    assert_response :redirect
+    assert_redirected_to redirect_path
+    assert_not_nil assigns[:artifact] 
+    assert_equal old_question_count + 1, Question.count
+  end
 end
