749119bc4d9a495fd459511705761da4710a9c6a
Steven Hammond <shammond@northpub.com>
2012-10-22 13:42:31 -0400

    Tests that autorized and unauthorized users see teh right things on the artifact show page.

--- test/functional/artifacts_controller_test.rb
+++ test/functional/artifacts_controller_test.rb
@@ -31,4 +31,51 @@ class ArtifactsControllerTest < ActionController::TestCase
     assert_template :daily
     assert_equal artifact, assigns(:artifact)
   end
+
+  context 'without an authorized user' do
+    setup do
+      @artifact = FactoryGirl.create(:artifact)
+      FactoryGirl.create(:question, artifact: @artifact)
+      FactoryGirl.create(:answered_question, artifact: @artifact)
+
+      get :show, id: @artifact.to_param
+    end
+
+    should 'be able to ask a new question' do
+      assert_select '#new_question'
+    end
+
+    should 'see answered questoins' do
+      assert_select '.answered-question', count: 1
+    end
+
+    should 'not see unanswered questions' do
+      assert_select '.unanswered-question', false
+    end
+  end
+
+    context 'with an authorized user' do
+    setup do
+      @user = FactoryGirl.create(:user)
+      sign_in @user
+
+      @artifact = FactoryGirl.create(:artifact)
+      FactoryGirl.create(:question, artifact: @artifact)
+      FactoryGirl.create(:answered_question, artifact: @artifact)
+
+      get :show, id: @artifact.to_param
+    end
+
+    should 'not be able to ask a new question' do
+      assert_select '#new_question', false
+    end
+
+    should 'see answered questions' do
+      assert_select '.answered-question', count: 1
+    end
+
+    should 'see unanswered questions' do
+      assert_select '.unanswered-question', count: 1
+    end
+  end
 end
