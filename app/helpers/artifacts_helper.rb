5bc4f802bb9c72af83d2950cd6f9d8a1df75b4de
Steven Hammond <shammond@northpub.com>
2012-12-16 22:51:04 -0500

    Fix spelling error in info sidebar.

--- app/helpers/artifacts_helper.rb
+++ app/helpers/artifacts_helper.rb
@@ -29,7 +29,7 @@ module ArtifactsHelper
     link_text, return_path = if request.referrer == root_url
       ['Return to Homepage', root_path]
     else
-      ['Reture to Search', artifacts_path(session[:search_params])]
+      ['Return to Search', artifacts_path(session[:search_params])]
     end
 
     link_to link_text, return_path, class: 'btn btn-inverse', style: 'margin-top: 10px;'
