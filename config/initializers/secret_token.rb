1d0321a07a2eefa65e6d66fcb13515cf081bc94f
Steven Hammond <shammond@northpub.com>
2013-02-04 07:57:45 -0500

    Move session secret out of source control. Duh.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HigginsCatalog::Application.config.secret_token = '80839fae090945c2da18844ed009097d4ced15ea98c912fa861aaf549720bb6b722388042bb1e68e21227a78742d0831bcbea34d28613024f15bb07fe1cc3bff'
+HigginsCatalog::Application.config.secret_token = ENV['SESSION_SECRET']
