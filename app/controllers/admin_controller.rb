0324e0e11dc198386cc5705dc68611d472efc268
Steven Hammond <shammond@patientslikeme.com>
2013-11-23 15:42:35 -0500

    Fixes #49: Add question summary info to admin index page.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,11 +7,17 @@ class AdminController < ApplicationController
     @artifact_count = Artifact.count
     @last_database_import = Artifact.order('created_at desc').first.created_at
     @unanswered_question_count = Question.unanswered.count
+
     @searches = {}
     @searches[:total] = SearchLog.count
     @searches[:today] = SearchLog.since(Date.today.to_time).count
     @searches[:last_seven_days] = SearchLog.since(7.days.ago).count
     @searches[:last_30_days] = SearchLog.since(30.days.ago).count
+
+    @questions = {}
+    @questions[:total] = Question.count
+    @questions[:unanswered] = Question.unanswered.count
+    @questions[:answered] = Question.answered.count
   end
 
   def user_report
